df90b56cc356c8836b92d292fb53890f37bcf748
廖金龙 <jinlongliao@foxmail.com>
2020-04-22 23:42:36 +0800

    # CyclicBarrier&CountDownLatch

--- src/test/java/com/liaojl/test/concurrent/Test2.java
+++ src/test/java/com/liaojl/test/concurrent/Test2.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 public class Test2 {
@@ -13,7 +15,7 @@ public class Test2 {
     public static final int SIZE = 5;
 
     /**
-     * 组团
+     * 同生共死
      *
      * @throws InterruptedException
      */
@@ -81,8 +83,117 @@ public class Test2 {
         }
     }
 
+    /**
+     * 有困难兄弟线上，我收人头
+     */
     @Test
-    public void cyclicBarrierTest() {
-
+    public void cyclicBarrierTest() throws InterruptedException {
+//        CyclicBarrier cyclicBarrier = new CyclicBarrier(SIZE);
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(SIZE,
+                () -> log.info("线程名：{} ， 时间：{} 坐等收人头！！！",
+                        Thread.currentThread().getName(),
+                        System.currentTimeMillis()));
+        Thread thread = null;
+        /**
+         * 当最后一个 await 完成 阻塞才完成
+         * true  最先执行 但后续的变为同步任务，他们要同步执行完成
+         * false 最后执行，其执行顺序为随意，任务的执行时间 是他们最后一个完成await 的时间
+         */
+        boolean iAmFirst = false;
+        for (int i = 0; i < SIZE; i++) {
+            if (2 > i && i < 4) {
+                /**
+                 * 当阻塞超时是 抛出错误异常，但不会影响后续执行，仅仅是错误提醒
+                 * <code>
+                 *    Exception in thread "线程2" java.util.concurrent.TimeoutException
+                 * 	at java.util.concurrent.CyclicBarrier.dowait(CyclicBarrier.java:257)
+                 * 	at java.util.concurrent.CyclicBarrier.await(CyclicBarrier.java:435)
+                 * 	at com.liaojl.test.concurrent.Test2$MyThread2.run(Test2.java:180)
+                 * 	at java.lang.Thread.run(Thread.java:748)
+                 * </code>
+                 */
+                thread = new Thread(new MyThread2(cyclicBarrier, 1, iAmFirst));
+            } else {
+                thread = new Thread(new MyThread2(cyclicBarrier, iAmFirst));
+            }
+            thread.setName("线程" + (i + 1));
+            thread.start();
+        }
+        Thread.sleep(5000);
     }
+
+    private static class MyThread2 implements Runnable {
+        private static final Logger log = LoggerFactory.getLogger(MyThread.class);
+        private final CyclicBarrier cyclicBarrier;
+        private boolean first;
+        private long timeout = 0;
+
+        /**
+         * @param cyclicBarrier
+         * @param first         是否最先执行
+         */
+        public MyThread2(CyclicBarrier cyclicBarrier, boolean first) {
+            this.cyclicBarrier = cyclicBarrier;
+            this.first = first;
+        }
+
+        /**
+         * @param cyclicBarrier
+         * @param timeout       延迟时间 纳秒
+         * @param first         是否最先执行
+         */
+        public MyThread2(CyclicBarrier cyclicBarrier, long timeout, boolean first) {
+            this.cyclicBarrier = cyclicBarrier;
+            this.timeout = timeout;
+            this.first = first;
+        }
+
+        /**
+         * When an object implementing interface <code>Runnable</code> is used
+         * to create a thread, starting the thread causes the object's
+         * <code>run</code> method to be called in that separately executing
+         * thread.
+         * <p>
+         * The general contract of the method <code>run</code> is that it may
+         * take any action whatsoever.
+         *
+         * @see Thread#run()
+         */
+        @SneakyThrows
+        @Override
+        public void run() {
+            long millis = (long) (Math.random() * 1000);
+            Thread.sleep(millis);
+            if (first) {
+                /**
+                 * 线程阻塞
+                 */
+                if (timeout < 1) {
+                    cyclicBarrier.await();
+                } else {
+                    cyclicBarrier.await(timeout, TimeUnit.NANOSECONDS);
+                    log.info("线程名：{}  lz不当炮灰，先走了！！！",
+                            Thread.currentThread().getName());
+                }
+            }
+            log.info("线程名：{} 总数：{},到达屏障总数：{},时间：{} 一起去打怪！！！",
+                    Thread.currentThread().getName(),
+                    cyclicBarrier.getParties(),
+                    cyclicBarrier.getNumberWaiting(),
+                    System.currentTimeMillis());
+            if (!first) {
+                /**
+                 * 线程阻塞
+                 */
+                if (timeout < 1) {
+                    cyclicBarrier.await();
+                } else {
+                    cyclicBarrier.await(timeout, TimeUnit.NANOSECONDS);
+                    log.info("线程名：{} lz不当炮灰，先走了！！！",
+                            Thread.currentThread().getName());
+                }
+            }
+        }
+    }
+
 }

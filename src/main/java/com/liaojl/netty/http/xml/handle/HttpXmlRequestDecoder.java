883297d951c18074e6e33f17492f9c8aad25f926
廖金龙 <jinlongliao@foxmail.com>
2020-04-28 13:54:54 +0800

    #  过期API 注销

--- src/main/java/com/liaojl/netty/http/xml/handle/HttpXmlRequestDecoder.java
+++ src/main/java/com/liaojl/netty/http/xml/handle/HttpXmlRequestDecoder.java
@@ -12,7 +12,7 @@ import io.netty.util.CharsetUtil;
 import java.util.List;
 
 import static io.netty.handler.codec.http.HttpResponseStatus.*;
-import static io.netty.handler.codec.http.HttpHeaders.Names.*;
+import static io.netty.handler.codec.http.HttpHeaderNames.*;
 import static io.netty.handler.codec.http.HttpVersion.*;
 
 public class HttpXmlRequestDecoder extends AbstractHttpXmlDecoder {
@@ -20,6 +20,7 @@ public class HttpXmlRequestDecoder extends AbstractHttpXmlDecoder {
     public HttpXmlRequestDecoder(Class clazz) {
         this(clazz, false);
     }
+
     //HttpXmlRequestDecoder有两个参数，分别为需要解码的对象的类型信息和是否打印HTTP消息体码流的码流开关，码流开关默认关闭。
     public HttpXmlRequestDecoder(Class clazz, boolean isPrint) {
         super(clazz, isPrint);
@@ -27,15 +28,15 @@ public class HttpXmlRequestDecoder extends AbstractHttpXmlDecoder {
 
     @Override
     protected void decode(ChannelHandlerContext arg0, Object o, List arg2) throws Exception {
-        FullHttpRequest arg1 = (FullHttpRequest)o;
+        FullHttpRequest arg1 = (FullHttpRequest) o;
         //首先对HTTP请求消息本身的解码结果进行判断，如果已经解码失败，再对消息体进行二次解码已经没有意义。
-        if (!arg1.getDecoderResult().isSuccess()) {
+        if (!arg1.decoderResult().isSuccess()) {
             //如果HTTP消息本身解码失败，则构造处理结果异常的HTTP应答消息返回给客户端。
             sendError(arg0, BAD_REQUEST);
             return;
         }
         //通过HttpXmlRequest和反序列化后的Order对象构造HttpXmlRequest实例，最后将它添加到解码结果List列表中。
-        HttpXmlRequest request = new HttpXmlRequest(arg1, decode0(arg0,arg1.content()));
+        HttpXmlRequest request = new HttpXmlRequest(arg1, decode0(arg0, arg1.content()));
         arg2.add(request);
     }
 

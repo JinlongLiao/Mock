c79d450401ec0556c3f34d6eb01ea09401d2f825
廖金龙 <jinlongliao@foxmail.com>
2020-04-28 11:19:03 +0800

    #  Spring 扫描

--- src/test/java/com/liaojl/test/annotation/spring/BeanFactoryTest.java
+++ src/test/java/com/liaojl/test/annotation/spring/BeanFactoryTest.java
@@ -1,9 +1,6 @@
 package com.liaojl.test.annotation.spring;
 
-import java.io.IOException;
-import java.util.LinkedHashSet;
-import java.util.Set;
-
+import com.liaojl.test.annotation.spring.scannere.MyTestScanner;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,14 +8,14 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ScannedGenericBeanDefinition;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.core.type.classreading.MetadataReader;
 import org.springframework.core.type.classreading.MetadataReaderFactory;
 import org.springframework.core.type.classreading.SimpleMetadataReaderFactory;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class BeanFactoryTest {
     private static final Logger log = LoggerFactory.getLogger(BeanFactoryTest.class);
@@ -30,7 +27,7 @@ public class BeanFactoryTest {
         ResourcePatternResolver resourcePatternResolver = new AnnotationConfigApplicationContext();
         //这里特别注意一下类路径必须这样写
         //获取指定包下的所有类
-        Resource[] resources = resourcePatternResolver.getResources("classpath*:kl/iam/*/*.class");
+        Resource[] resources = resourcePatternResolver.getResources("classpath*:com/liaojl/test/*/*.class");
 
         MetadataReaderFactory metadata = new SimpleMetadataReaderFactory();
         for (Resource resource : resources) {
@@ -49,6 +46,16 @@ public class BeanFactoryTest {
                 log.info("classname:{}", classname);
             }
         }
+    }
+
+    @Test
+    public void scanner2() throws IOException, ClassNotFoundException {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        MyTestScanner scanner = new MyTestScanner(applicationContext);
+        scanner.setAnnotationClass(TestAnnotation.class);
+        scanner.registerFilters();
+        int scan = scanner.scan("com.liaojl.test");
+        log.info("扫描到的类数量：{}", scan);
     }
 
 }

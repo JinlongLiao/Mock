f2a13a36c9f9552be0641459b353d5362c111420
廖金龙 <jinlongliao@foxmail.com>
2020-04-30 01:11:48 +0800

    #  KMP 查找

---
+++ src/test/java/com/liaojl/test/algorithm/kmp/KpmTest.java
@@ -0,0 +1,113 @@
+package com.liaojl.test.algorithm.kmp;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.validation.constraints.NotNull;
+
+public class KpmTest {
+    private static final Logger log = LoggerFactory.getLogger(KpmTest.class);
+
+    @Test
+    public void test() {
+        String ps = "aab";
+        String target = "1gg2abababcabaabbca0";
+        kmp(target, ps);
+    }
+
+    /**
+     * </pre></blockquote>
+     * 1. 首先获取{@code ps}  的next[]
+     * <p>
+     * 例如 ：[0,1,0]
+     * <p>
+     * {@code target} caaab
+     * {@code ps} aab
+     * 1:caaab
+     * aab
+     * 没有相等的移动一位
+     * 2:caaab
+     * aab
+     * 有两位相等 -> next[2]=0 为0时 +1
+     * 2:caaab
+     * aab
+     * 匹配成功
+     *
+     * </pre></blockquote>
+     *
+     * @param target 目标字符串
+     * @param ps     匹配字符串
+     */
+    public void kmp(String target, String ps) {
+
+        int[] next = getNext(ps);
+        char[] psChars = ps.toCharArray();
+        char[] targetChars = target.toCharArray();
+        log.info("next: {}", next);
+
+        int index = -1;
+        int i = 0;
+        for (int j = 0; j < psChars.length; ) {
+            if (i + j > targetChars.length - 1) {
+                break;
+            }
+            if (targetChars[i + j] == psChars[j]) {
+                ++j;
+                if (j == psChars.length) {
+                    index = i;
+                }
+            } else {
+                i += next[j] == 0 ? 1 : next[j];
+                j = 0;
+            }
+        }
+        log.info("index: {}", index + 1);
+
+    }
+
+    /**
+     * <blockquote><pre>
+     * 例如：ababbc 获取next[]
+     * next[0] 默认为0
+     * 1：ababbc
+     *     ababbc
+     *    00
+     * 总共匹配0 获取移动量 next[0] 为0是 自动加一
+     * 2：ababbc
+     *      ababbc
+     *    0012
+     * 总共匹配2 获取移动量 next[2]=1
+     * 3:ababbc
+     *       ababbc
+     *   00120
+     * 总共匹配0 获取移动量 next[0] 为0是 自动加一
+     * 4:ababbc
+     *        ababbc
+     *  001200
+     *  结束
+     * </pre></blockquote>
+     *
+     * @param ps
+     * @return
+     */
+    public int[] getNext(@NotNull String ps) {
+        int length = ps.length();
+        char[] chars = ps.toCharArray();
+        int[] next = new int[length];
+        next[0] = 0;
+        if (length > 1) {
+            int i = 1;
+            for (int j = 0; i + j < length; ) {
+                if (chars[i + j] == chars[j]) {
+                    next[i + j] = j + 1;
+                    j++;
+                } else {
+                    i += next[j] == 0 ? 1 : next[j];
+                    j = 0;
+                }
+            }
+        }
+        return next;
+    }
+}
--- src/test/java/com/liaojl/test/speed/JavaTest.java
+++ src/test/java/com/liaojl/test/speed/JavaTest.java
@@ -97,4 +97,92 @@ public class JavaTest {
         log.info("hessionByte:{}", hessionByte);
         log.info("hessionByte :{}", System.currentTimeMillis() - t2);
     }
+
+    private static final int SPLIT_SIZE = 2;
+
+    @Test
+    public void suString() {
+        String charSequence = "KMP算法是一种改进的字符串匹配算法，由D.E.Knuth，J.H.Morris和V.R.Pratt提出的，因此人们称它为克努特—莫里斯—普拉特操作（简称KMP算法）。KMP算法的核心是利用匹配失败后的信息，尽量减少模式串与主串的匹配次数以达到快速匹配的目的。具体实现就是通过一个next()函数实现，函数本身包含了模式串的局部匹配信息。";
+        charSequence = charSequence + charSequence + charSequence + charSequence;
+        charSequence = charSequence + charSequence + charSequence + charSequence;
+        charSequence = charSequence + charSequence + charSequence + charSequence;
+        int length = charSequence.length();
+        //两个分割
+        long t1 = System.currentTimeMillis();
+
+        for (int i = 0; i < length; i += SPLIT_SIZE) {
+            String substring = charSequence.substring(i, (i + SPLIT_SIZE) > length ? length : (i + SPLIT_SIZE));
+//            log.info("substring:{}", substring);
+        }
+        long time1 = System.currentTimeMillis() - t1;
+        log.info("substring:{}", time1);
+
+        long t2 = System.currentTimeMillis();
+        char[] toCharArray = charSequence.toCharArray();
+        int size = 0;
+        while (size + SPLIT_SIZE < length) {
+            String str = new String(toCharArray, size, SPLIT_SIZE);
+            size += SPLIT_SIZE;
+//            log.info("toCharArray:{}", str);
+        }
+        long time2 = System.currentTimeMillis() - t2;
+        log.info("toCharArray:{}", time2);
+
+
+    }
+
+    @Test
+    public void KMP() {
+        String ts = "ABCSBUGEBDKFCBCDLD", ps = "ABB";
+        char[] t = ts.toCharArray();
+        char[] p = ps.toCharArray();
+        // 主串的位置
+        int i = 0;
+        // 模式串的位置
+        int j = 0;
+        int[] next = getNext(p);
+        while (i < t.length && j < p.length) {
+            // 当j为-1时，要移动的是i，当然j也要归0
+            if (j == -1 || t[i] == p[j]) {
+                i++;
+                j++;
+            } else {
+                // i不需要回溯了
+                // i = i - j + 1;
+                // j回到指定位置
+                j = next[j];
+            }
+        }
+        int result;
+        if (j == p.length) {
+
+            result = i - j;
+
+        } else {
+
+            result = -1;
+
+        }
+        log.info("result:{}", result);
+    }
+
+    public int[] getNext(char[] ps) {
+        int[] next = new int[ps.length];
+        next[0] = -1;
+        int j = 0;
+        int k = -1;
+        while (j < ps.length - 1) {
+            if (k == -1 || ps[j] == ps[k]) {
+                // 当两个字符相等时要跳过
+                if (ps[++j] == ps[++k]) {
+                    next[j] = next[k];
+                } else {
+                    next[j] = k;
+                }
+            } else {
+                k = next[k];
+            }
+        }
+        return next;
+    }
 }

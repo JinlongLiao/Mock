dbd1b9f74c32d98b696869d86efc354be032ea92
廖金龙 <jinlongliao@foxmail.com>
2020-05-06 23:23:17 +0800

    #  OpenJdk jol

---
+++ src/test/java/com/liaojl/test/vm/ObjectTest.java
@@ -0,0 +1,21 @@
+package com.liaojl.test.vm;
+
+import org.junit.Test;
+import org.openjdk.jol.info.ClassLayout;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ObjectTest {
+    private static final Logger log = LoggerFactory.getLogger(ObjectTest.class);
+
+
+    @Test
+    public void test1() throws InterruptedException {
+//        Thread.sleep(5000);
+        Object o = new Object();
+        log.info("{}", ClassLayout.parseInstance(o).toPrintable());
+        synchronized (o) {
+            log.info("{}", ClassLayout.parseInstance(o).toPrintable());
+        }
+    }
+}

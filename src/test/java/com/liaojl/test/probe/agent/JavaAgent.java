f2c0687ca3724059e4985aeb63d1c50062676313
廖金龙 <jinlongliao@foxmail.com>
2020-05-11 17:37:52 +0800

    # Javassit 字节码修改

--- src/test/java/com/liaojl/test/probe/agent/JavaAgent.java
+++ src/test/java/com/liaojl/test/probe/agent/JavaAgent.java
@@ -4,11 +4,13 @@ import com.sun.tools.attach.AgentInitializationException;
 import com.sun.tools.attach.AgentLoadException;
 import com.sun.tools.attach.AttachNotSupportedException;
 import com.sun.tools.attach.VirtualMachine;
+import javassist.*;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class JavaAgent {
     private static final Logger log = LoggerFactory.getLogger(JavaAgent.class);
@@ -33,5 +35,24 @@ public class JavaAgent {
     @Test
     public void test2() {
         log.info(String.valueOf(new TransClass().getNumber()));
+    }
+
+    @Test
+    public void javassitTest() throws NotFoundException, CannotCompileException {
+        ClassPool classPool = ClassPool.getDefault();
+        URL url = classPool.find("com.liaojl.test.probe.agent.TransClass");
+        log.info("URL :{}", url);
+        CtClass ctClass = classPool.get("com.liaojl.test.probe.agent.TransClass");
+        CtMethod[] methods = ctClass.getMethods();
+        for (CtMethod method : methods) {
+            log.info("method：{}", method.getName());
+        }
+        CtMethod getNumber = ctClass.getDeclaredMethod("getNumber");
+        getNumber.insertBefore("log.info(\"Javassit 新增\");");
+        ctClass.toClass();
+        TransClass transClass = new TransClass();
+        String number = transClass.getNumber();
+        String msg = String.valueOf(number);
+        log.info(msg);
     }
 }
--- src/test/java/com/liaojl/test/probe/agent/TransClass.java
+++ src/test/java/com/liaojl/test/probe/agent/TransClass.java
@@ -1,6 +1,11 @@
 package com.liaojl.test.probe.agent;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TransClass {
+    private static final Logger log = LoggerFactory.getLogger(TransClass.class);
+
     public String getNumber() {
         return "我是原始的";
     }

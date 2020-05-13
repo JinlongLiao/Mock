b843b647895783c76b10581a62f0cd4208a58a21
廖金龙 <jinlongliao@foxmail.com>
2020-05-13 21:58:22 +0800

    # try catch finally 字节码

--- src/test/java/com/liaojl/test/vm/ObjectTest.java
+++ src/test/java/com/liaojl/test/vm/ObjectTest.java
@@ -20,11 +20,58 @@ public class ObjectTest {
     }
 
     @Test
-    public void testNumberToSquare() {
+    public void testNumberToNSquare() {
         int i = Integer.numberOfLeadingZeros(10);
         int n = -1 >>> i;
 
         log.info("I :{}", i);
         log.info("N :{}", n + 1);
+    }
+
+    /**
+     * 0 iconst_1
+     * 1 istore_1
+     * 2 iinc 1 by 1
+     * 5 getstatic #12 <java/lang/System.out>
+     * 8 iload_1
+     * 9 invokestatic #13 <java/lang/String.valueOf>
+     * 12 iconst_0
+     * 13 anewarray #2 <java/lang/Object>
+     * 16 invokevirtual #14 <java/io/PrintStream.printf>
+     * 19 pop
+     * 20 goto 61 (+41)
+     * 23 astore_2
+     * 24 iinc 1 by 1
+     * 27 getstatic #12 <java/lang/System.out>
+     * 30 iload_1
+     * 31 invokestatic #13 <java/lang/String.valueOf>
+     * 34 iconst_0
+     * 35 anewarray #2 <java/lang/Object>
+     * 38 invokevirtual #14 <java/io/PrintStream.printf>
+     * 41 pop
+     * 42 return
+     * 43 astore_3
+     * 44 getstatic #12 <java/lang/System.out>
+     * 47 iload_1
+     * 48 invokestatic #13 <java/lang/String.valueOf>
+     * 51 iconst_0
+     * 52 anewarray #2 <java/lang/Object>
+     * 55 invokevirtual #14 <java/io/PrintStream.printf>
+     * 58 pop
+     * 59 aload_3
+     * 60 athrow
+     * 61 return
+     */
+    @Test
+    public void byteCodeTest() {
+        int a = 1;
+        try {
+            a++;
+        } catch (Exception e) {
+            ++a;
+            return;
+        } finally {
+            System.out.printf(String.valueOf(a));
+        }
     }
 }
